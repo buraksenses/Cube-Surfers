1cf062ef1bde1be45546a19340e2bf8dda1762ad
Burak Şenses <buraksenseees@gmail.com>
2022-11-05 17:26:43 +0300

    Default Solver Iterations increased to prevent jittery movement.

--- Assets/0 Scripts/Managers/StackManager.cs
+++ Assets/0 Scripts/Managers/StackManager.cs
@@ -22,20 +22,21 @@ namespace CubeSurfers.Managers
         public void Stack(Transform cube)
         {
             if(!cube.CompareTag("First Cube"))
-                cube.position = new Vector3(lastCubePos.position.x, lastCubePos.position.y + ((cube.transform.localScale.y + .05f) * stackableCubes.Count), lastCubePos.position.z);
+                cube.position = 
+                    new Vector3(lastCubePos.position.x, lastCubePos.position.y + ((cube.transform.localScale.y + .05f) * stackableCubes.Count), lastCubePos.position.z);
             stackableCubes.Add(cube);
 
             //StartCoroutine(OscillateRoutine());
         }
 
-        // private IEnumerator OscillateRoutine()
-        // {
-        //     for (int i = 0; i < stackableCubes.Count; i++)
-        //     {
-        //         stackableCubes[i].DOPunchScale(new Vector3(1, 0, 1), .3f);
-        //         yield return new WaitForSeconds(.1f);
-        //     }
-        // }
+        private IEnumerator OscillateRoutine()
+        {
+            for (int i = 0; i < stackableCubes.Count; i++)
+            {
+                stackableCubes[i].DOPunchScale(new Vector3(1, 0, 1), .3f);
+                yield return new WaitForSeconds(.1f);
+            }
+        }
 
         public void Unstack(Transform cube)
         {
